8368b266fe8fd015a087b0a2256025c6b1337f8e
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2020-10-09 22:43:20 +0900

    first commit

---
+++ src/components/lifting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Lifting: React.FC = () => {
+
+    return (
+        <>
+            <div>sample</div>
+        </>
+    );
+  };
+
+  export default Lifting;
---
+++ src/pages/lifting.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Lifting from '../components/lifting';
+
+const LiftingPage: React.FC = () => {
+
+    return (
+        <>
+            <div>sample</div>
+            <Lifting/>
+        </>
+    );
+  };
+
+  export default LiftingPage;
